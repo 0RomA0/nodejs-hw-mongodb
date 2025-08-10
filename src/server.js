6f6a7a6d62944b8e11a89939e42751ac93fdb957
Roma Galyh <romagalyh@gmail.com>
2025-08-11 01:33:02 +0300

    add hw-№7 Node.js

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 
 
@@ -28,6 +29,8 @@ export function setupServer() {
     );
 
     app.use('/uploads', express.static(UPLOAD_DIR));
+
+      app.use('/api-docs', swaggerDocs());
 
     app.use(router);
 
